104cf31f9eb63b39b549dd27da6a82c68e08054f
屠虎 <tuhu@alibaba-inc.com>
2019-02-07 10:10:55 +0800

    five question
    Change-Id: I32f5f6297a0f8bdb5814a0908927cd2e86cd2805

---
+++ src/6_isPalindrome.py
@@ -0,0 +1,9 @@
+"""
+https://leetcode.com/problems/palindrome-number/
+"""
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
---
+++ src/7_reverseList.py
@@ -0,0 +1,10 @@
+"""
+https://leetcode.com/problems/reverse-linked-list/
+"""
+class Solution(object):
+
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
